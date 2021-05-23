45732c59be76911fa22c637097ae75ec66f807da
SpearZero <spearzero0@gmail.com>
2021-05-23 23:34:36 +0900

    전략 패턴

---
+++ README.md
@@ -0,0 +1,5 @@
+# 헤드 퍼스트 디자인 패턴
+
+> https://book.naver.com/bookdb/book_detail.nhn?bid=1882446 
+
+- [전략 패턴](./src/main/java/designpatterns/strategy)
---
+++ src/main/java/designpatterns/strategy/FlyNoWay.java
@@ -0,0 +1,9 @@
+package designpatterns.strategy;
+
+public class FlyNoWay implements FlyBehavior{
+
+    @Override
+    public void fly() {
+        System.out.println("저는 못 날아요");
+    }
+}
